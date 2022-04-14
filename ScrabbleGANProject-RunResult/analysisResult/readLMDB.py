799cd70dba345a58594af84cc2686d17a0be1c8c
KaixuanLee <flyfoxkx@GMAIL.COM>
2022-04-14 15:31:45 -0400

    GAN_nowmodel_result_image

--- ScrabbleGANProject-RunResult/analysisResult/readLMDB.py
+++ ScrabbleGANProject-RunResult/analysisResult/readLMDB.py
@@ -3,53 +3,23 @@
 from base64 import encode
 from PIL import Image
 import lmdb
-import caffe2
 
-def read_from_lmdb(lmdb_path,img_save_to):
- try:
-  lmdb_env=lmdb.open(lmdb_path, map_size=3221225472)
-  lmdb_txn=lmdb_env.begin()
-  lmdb_cursor=lmdb_txn.cursor()
-  datum=caffe2.Datum()
- 
-  datum_index=0
-  for key,value in lmdb_cursor:
-    datum.ParseFromString(value)
-    label=datum.label
-    data=datum.data
-    channel=datum.channels
-    print ('Datum channels: %d' % datum.channels)
-    print ('Datum width: %d' % datum.width)
-    print ('Datum height: %d' % datum.height)
-    print ('Datum data length: %d' % len(datum.data))
-    print ('Datum label: %d' % datum.label)
- 
-    size=datum.width*datum.height
-    pixles1=datum.data[0:size]
-    pixles2=datum.data[size:2*size]
-    pixles3=datum.data[2*size:3*size]
-    #Extract images of different channel
-    image1=Image.frombytes('L', (datum.width, datum.height), pixles1)
-    image2=Image.frombytes('L', (datum.width, datum.height), pixles2)
-    image3=Image.frombytes('L', (datum.width, datum.height), pixles3)
-    
-    image4=Image.merge("RGB",(image3,image2,image1))
-    image4.save(img_save_to+str(key)+".jpg")
-    datum_index+=1
-    print ("extracted")
- 
- finally:
-   lmdb_env.close()
-
-def read_from_lmdb2(lmdb_path):
-    env_db = lmdb.open(lmdb_path) 
-    txn = env_db.begin() 
-    print(txn.get(("image-000000001").encode()))
-    print()
-    for key, value in txn.cursor():
-        print(key, value) 
-        print(123)
-    env_db.close()
+from path import Path
+from PIL import Image
+from io import BytesIO
+import os
+def read_from_lmdb(lmdb_path):
+    env_db = lmdb.open(lmdb_path, readonly=True) 
+    with env_db.begin() as txn:
+      for i in range(1000):
+        #i = i + 115000
+        img_key = 'image-%09d' % i
+        print(img_key)
+        data = txn.get(img_key.encode("utf-8"))
+        if(data != None):
+          img_name = str(i)+'.jpg'
+          with open(os.path.join("../result/"+img_name),'wb') as f:
+              f.write(data)
 
 
-read_from_lmdb2("E:\data")
+read_from_lmdb("E:/MyUniversity/Advaced Intro to Machine Learning _CSCI642/final project/convolutional-handwriting-gan-master/lmdb_files/finalProjectDemo_IAM_concat100k")
